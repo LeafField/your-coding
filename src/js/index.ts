b4abe5058c8c71e967283a88eddfd72fd017db21
LeafField <109466326+LeafField@users.noreply.github.com>
2023-03-31 23:18:12 +0900

    questionセクションの完成とアコーディオンパネルの機能開発

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,9 +1,11 @@
 import "ress";
 import "../styles/style.scss";
+import { accordionAnimation } from "./accordion";
 import { hamburgerActions } from "./hamburger";
 import { intersection } from "./intersection";
 
 window.addEventListener("DOMContentLoaded", () => {
   hamburgerActions();
   intersection(".js-intersection");
+  accordionAnimation();
 });
