b88d45f6aa12739e15e23ef97e60fa6a1a82e2aa
LeafField <109466326+LeafField@users.noreply.github.com>
2023-04-05 14:13:48 +0900

    完成しました

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,11 +6,18 @@ import { hamburgerActions } from "./hamburger";
 import { intersection } from "./intersection";
 
 import Swiper, { Autoplay } from "swiper";
+import { scrollAnimation } from "./scroll";
 
 window.addEventListener("DOMContentLoaded", () => {
   hamburgerActions();
   intersection(".js-intersection");
   accordionAnimation();
+});
+
+const contacts = document.querySelectorAll<HTMLAnchorElement>(".contact-btn");
+
+contacts.forEach((element) => {
+  element.addEventListener("click", (e) => scrollAnimation(e));
 });
 
 new Swiper(".swiper", {
@@ -23,7 +30,7 @@ new Swiper(".swiper", {
   grabCursor: true,
   speed: 600,
   autoplay: {
-    delay: 4000,
+    delay: 5000,
     disableOnInteraction: false,
   },
   breakpoints: {
